ad9360cc160d2a0164724b1db340f43719cb7bcd
Stas <www.stas1996@mail.ru>
2023-04-01 13:24:29 +0300

    Задание 47

---
+++ Задание 47/Program.cs
@@ -0,0 +1,28 @@
+﻿void ImputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) matrix[i, j] = new Random().Next(0, 11);
+    } 
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    } 
+}
+
+Console.Clear();
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите x: ");
+int x = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[m, x];
+ImputMatrix(matrix);
+PrintMatrix(matrix);
