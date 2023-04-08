0074cfe25c3b3fdbaaec4bdb6d781cfef279b2aa
Stas <www.stas1996@mail.ru>
2023-04-08 19:17:00 +0300

    Задача 66

---
+++ Задача 66/Program.cs
@@ -0,0 +1,14 @@
+﻿int f(int n, int m)
+{
+    int sum = 0;
+    if(m > n)
+        return 0;
+    return m + f(n, m + 1);
+}
+
+Console.Clear();
+Console.Write("Ввидите M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Ввидите N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n, m));
