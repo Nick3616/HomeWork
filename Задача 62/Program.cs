ac80674e0cd4e45391528fcbc91f66eab3c8d67e
Stas <www.stas1996@mail.ru>
2023-04-08 19:16:40 +0300

    Задача 62

---
+++ Задача 62/Program.cs
@@ -0,0 +1,11 @@
+﻿string f(int n)
+{
+    if(n == 0)
+        return "";
+    return $"{n} " + f(n - 1);
+}
+
+Console.Clear();
+Console.Write("Ввидите N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n));
