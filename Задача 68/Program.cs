07f736ce7973f61684b8735686386a98bdd8cb7f
Stas <www.stas1996@mail.ru>
2023-04-08 19:17:10 +0300

    Задача 68

---
+++ Задача 68/Program.cs
@@ -0,0 +1,16 @@
+﻿    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (n == 0)
+            return Ackermann(m - 1, 1);
+        else
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+Console.Clear();
+Console.Write("Ввидите M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Ввидите N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Ackermann(m, n));
